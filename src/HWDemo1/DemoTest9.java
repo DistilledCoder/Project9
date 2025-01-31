3a56188a755cddd6b78589cfa0c864acf161dbf1
Uncle Ricco <eric4cali@gmail.com>
2025-01-30 22:18:45 -0800

    Update DemoTest9.java

--- src/HWDemo1/DemoTest9.java
+++ src/HWDemo1/DemoTest9.java
@@ -4,6 +4,8 @@ public class DemoTest9 {
 	public static void main( String [] args) {
 		
 		System.out.println("Let's do this again...learn this by repitition");
+
+		System.out.println("Do 10,000 reps and you will be going in the right direction");
 		
 	}
 
