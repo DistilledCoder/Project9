53d5e87d6df46576f4fc299a0b8aafbe5531c14f
ericw <ericw@UncleRicco.san.rr.com>
2025-01-30 22:20:13 -0800

    Small edit to commit

--- src/HWDemo1/DemoTest9.java
+++ src/HWDemo1/DemoTest9.java
@@ -3,7 +3,7 @@ package HWDemo1;
 public class DemoTest9 {
 	public static void main( String [] args) {
 		
-		System.out.println("Let's do this again...learn this by repitition");
+		System.out.println("Let's do this again...learn this by repetition");
 
 		System.out.println("Do 10,000 reps and you will be going in the right direction");
 		
